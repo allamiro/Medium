66a37e65c04c831a2bb5cd2745fc44eaf7bdf743
Tamir Suliman <allamir2002@yahoo.com>
2024-10-04 18:15:40 +0000

    Creating the file

---
+++ Genetic Algorithm for Lennard-Jones Clusters in Cybersecurity & Machine Learning/ga-ml-phishing-lj.py
@@ -0,0 +1,9 @@
+# Importing necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from deap import base, creator, tools, algorithms
+import random
+import numpy as np
